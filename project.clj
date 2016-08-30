9fb59972576f3b07d8f5777e1073dbbb64caf2c0
Ian McEwen <mian@cyverse.org>
2016-08-30 12:33:11 -0700

    Update URL, add pom.xml.asc to gitignore

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.cyverse/debug-utils "2.8.1-SNAPSHOT"
   :description "Common Debugging Utilities for Clojure Projects"
-  :url "https://github.com/cyverse/DE"
+  :url "https://github.com/cyverse-de/debug-utils"
   :license {:name "BSD"
             :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
   :dependencies [[org.clojure/clojure "1.8.0"]
