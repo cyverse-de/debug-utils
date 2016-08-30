d70878532d6fb2739d675c8432d2d2700d8104c9
Ian McEwen <mian@cyverse.org>
2016-08-30 14:01:26 -0700

    Add :deploy-repositories for debug-utils

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "https://github.com/cyverse-de/debug-utils"
   :license {:name "BSD"
             :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]])
