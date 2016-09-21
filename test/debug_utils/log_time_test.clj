b6e75399dc03ec20b9631b505cabb9b05e9f2875
Ian McEwen <mian@cyverse.org>
2016-09-21 13:33:55 -0700

    make tests work

--- test/debug_utils/log_time_test.clj
+++ test/debug_utils/log_time_test.clj
@@ -1,6 +1,6 @@
-(ns debug-utils.core-test
+(ns debug-utils.log-time-test
   (:require [clojure.test :refer :all]
-            [debug-utils.core :refer :all]))
+            [debug-utils.log-time :refer :all]))
 
 (deftest a-test
   (testing "Dummy Test"
